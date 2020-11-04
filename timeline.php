710c6449d78ca59261d78552863f04a47964f34e
skkkzhang <shuningz@usc.edu>
2020-11-04 10:47:27 -0800

    change jquery to uncompressed version & modify tempHtml.html

--- timeline.php
+++ timeline.php
@@ -30,7 +30,7 @@ $connect->close();
         <link rel="icon" type="image/png" href="images/favicons/event.ico">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
         
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
         <script src="js/timeline.min.js"></script>
         <link rel="stylesheet" href="css/timeline.min.css" />
         <link rel="stylesheet" type="text/css" href="style.css">
