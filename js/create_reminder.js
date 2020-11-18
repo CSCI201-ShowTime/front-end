81255c635623ff9ee8634568097311cd67e7fb68
Zhian Li <zhianli@usc.edu>
2020-11-19 02:37:16 +0800

    fix bugs

--- js/create_reminder.js
+++ js/create_reminder.js
@@ -14,4 +14,47 @@ document.querySelector("form").onsubmit = function(e) {
 		document.querySelector("#date + .error").style.display = "none";
 	}
 	let notes = document.querySelector("#notes").value;
+	
+	var myDate=new Date();
+	myDate.setDate(myDate.getDate()+1);
+	
+	let priority = document.querySelector("#priority").value;
+	
+	$.get("/api/userid", { email: "email" }, function(data){
+  		$.ajax({
+		method: "POST",
+		url: "/api/event/reminder",
+		// Fixed: contentType must be explicitly defined if submitted as a form
+		// form auto-submits as "application/x-www-form-urlencoded"
+		contentType: "application/json",
+		// Fixed: for JSON, data must be explicitly parsed
+		data: JSON.stringify({
+			remind_time: myDate,
+			priority: priority,
+//			eventid: ,
+    		userid: data,
+    		start: new Date(),
+			end: deadline,
+    		title: title,
+			description: notes,
+			visibility: 1,
+			type: "reminder",
+			location: "Los Angeles"
+		})
+		})
+		.done(function( data, textStatus, jqXHR ) {
+			// on success logic, redirect?			
+			window.location.href = "/timeline";
+		})
+		.fail(function( jqXHR, textStatus, errorThrown ) {
+			alert("Or here");
+			if (jqXHR.status == 401) {
+				alert( "Unmatched email and password!" );
+			} else {
+				console.log("error!");
+			}
+		});
+	});
+
+
 }
