2bad02d28c1b5b25863284cec21c54e4debce4c4
zheweixu <69445296+zheweixu@users.noreply.github.com>
2020-11-23 14:10:46 +0800

    no php

--- js/budget-google-chart.js
+++ js/budget-google-chart.js
@@ -13,27 +13,12 @@ function initChart() {
     gData = new google.visualization.DataTable();
     gData.addColumn('string', 'Topping');
     gData.addColumn('number', 'Slices');
-    /*gData.addRows([
-        ['Food', 3],
-        ['Clothes', 1],
-        ['Fun', 1],
-        ['Gifts', 1],
-        ['Transportation', 2],
-        ['Pet', 1]
-    ]);*/
 }
 
 // Callback that creates and populates a data table,
 // instantiates the pie chart, passes in the data and
 // draws it.
 function drawChart() {
-    // 	var jsonData = $.ajax({
-    // url: "<?php echo base_url() . 'index.php/main/lihat_graph' ?>",
-    // dataType: "json",
-    // async: false
-    // }).responseText;
-        // var data = new google.visualization.DataTable(jsonData);
-
     // Set chart options
     var options = {pieHole: 0.4,
                         'chartArea': {'width': '100%', 'height': '80%'},
